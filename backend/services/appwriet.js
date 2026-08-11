0c5c902c318b189015099fdde75059ce7ce7ad80
Anthony Wai <waidanielchol478@gmail.com>
2026-08-11 17:46:45 +0300

    chore: remove deprecated properties from app.json

--- backend/services/appwriet.js
+++ backend/services/appwriet.js
@@ -6,19 +6,30 @@
 const { ID, Query } = require("node-appwrite");
 const { databaseManager } = require("../src/database/connection");
 
+function ensureAppwriteInitialized(clientName) {
+  const client = databaseManager[clientName];
+  if (!client) {
+    throw new Error(
+      `Appwrite ${clientName} client is not initialized. ` +
+        "Ensure Appwrite is reachable and backend startup completed successfully.",
+    );
+  }
+  return client;
+}
+
 Object.defineProperty(module.exports, "db", {
   enumerable: true,
-  get: () => databaseManager.databases,
+  get: () => ensureAppwriteInitialized("databases"),
 });
 
 Object.defineProperty(module.exports, "users", {
   enumerable: true,
-  get: () => databaseManager.users,
+  get: () => ensureAppwriteInitialized("users"),
 });
 
 Object.defineProperty(module.exports, "account", {
   enumerable: true,
-  get: () => databaseManager.account,
+  get: () => ensureAppwriteInitialized("account"),
 });
 
 module.exports.ID = ID;
